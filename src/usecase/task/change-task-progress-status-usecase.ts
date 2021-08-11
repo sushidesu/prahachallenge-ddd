6dfb179c6de4721b39fc192679b90d15fc0b464f
sushidesu <hugemerge@gmail.com>
2021-08-11 18:33:05 +0900

    課題のユースケースを整理

---
+++ src/usecase/task/change-task-progress-status-usecase.ts
@@ -0,0 +1,12 @@
+export class ChangeTaskProgressStatusUsecase {
+  /**
+   * 課題の更新 (課題ステータスの変更)
+   *
+   * どのステータスに変更するかは引数で指定する
+   */
+  exec(): void {
+    // - 変更したい課題 entity を取得
+    // - 課題 entity の進捗ステータスを変更
+    // - 課題 entity を保存
+  }
+}
