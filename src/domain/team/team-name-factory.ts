54b1c9085ccfbe9b4da384ba53c56690a45188fa
sushidesu <hugemerge@gmail.com>
2021-09-20 21:43:46 +0900

    refactor: 余分なコードを削除

--- src/domain/team/team-name-factory.ts
+++ src/domain/team/team-name-factory.ts
@@ -13,10 +13,6 @@ export class TeamNameFactory extends DomainService<"team-name-factory"> {
     // 全チームを取得
     // チーム名で並び替え
     const teams = await this.teamRepository.getAllTeamList({ sort: "name" })
-    // チームがなにもない場合 "1" で作成
-    if (!teams.length) {
-      return TeamName.createFromFactory("1")
-    }
     // 最後のチームの次のチーム名を取得
     const nextTeamNumber = teams.length + 1
     // 次が1000の場合エラー
