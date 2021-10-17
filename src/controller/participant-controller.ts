a4be7e97cbaf007907fb7c79f2a65095a44d9e8b
sushidesu <hugemerge@gmail.com>
2021-10-17 20:35:09 +0900

    コントローラのエラーハンドリング

--- src/controller/participant-controller.ts
+++ src/controller/participant-controller.ts
@@ -4,12 +4,23 @@ import { JoinPrahaChallengeInputData } from "../usecase/participant/join-praha-c
 
 export class ParticipantController {
   constructor(private joinPrahaChallengeUsecase: JoinPrahaChallengeUsecase) {}
-  public create: RequestHandler = async (_, res) => {
-    const inputData = new JoinPrahaChallengeInputData({
-      name: "",
-      email: "",
-    })
-    await this.joinPrahaChallengeUsecase.exec(inputData)
-    res.json({ result: "true" })
+  public create: RequestHandler = async (req, res, next) => {
+    try {
+      const inputData = new JoinPrahaChallengeInputData({
+        name: "",
+        email: "",
+      })
+      await this.joinPrahaChallengeUsecase.exec(inputData)
+      res.json({ message: "success!" })
+    } catch (err) {
+      console.error(err)
+      if (err instanceof Error) {
+        res.status(500).json({ message: err.message })
+      } else {
+        res.status(500).json({ message: "unexpected error occurred." })
+      }
+    } finally {
+      next()
+    }
   }
 }
