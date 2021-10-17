c1edd0db0ae06e59a2d7549deb7793e214d1e9ee
sushidesu <hugemerge@gmail.com>
2021-10-17 20:34:22 +0900

    json-parserを有効化

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { participantRouter } from "./config/participant-router"
 const app = express()
 const PORT = 8000
 
+app.use(express.json())
+
 app.get("/", (_, res) => {
   res.send("hello")
 })
