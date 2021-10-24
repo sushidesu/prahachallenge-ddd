52709ec41b3d70bad11881027f0166c345e8fb75
sushidesu <hugemerge@gmail.com>
2021-10-24 17:49:00 +0900

    getAllPairListを実装

--- src/infra/pair/pair-repository.ts
+++ src/infra/pair/pair-repository.ts
@@ -1,16 +1,45 @@
+import { Context } from "../shared/context"
 import { IPairRepository } from "../../domain/pair/interface/pair-repository"
 import { Pair } from "../../domain/pair/pair"
+import { PairId } from "../../domain/pair/pair-id"
+import { PairName } from "../../domain/pair/pair-name"
+import { TeamId } from "../../domain/team/team-id"
+import { ParticipantId } from "../../domain/participant/participant-id"
 
 export class PairRepository implements IPairRepository {
+  constructor(private readonly context: Context) {}
+
   async insert(): Promise<void> {
     // TODO:
   }
   async update(): Promise<void> {
     // TODO:
   }
+
   async getAllPairList(): Promise<Pair[]> {
-    // TODO:
-    return []
+    const result = await this.context.prisma.pair.findMany({
+      include: {
+        teams: {
+          select: {
+            id: true,
+          },
+        },
+        users: {
+          select: {
+            id: true,
+          },
+        },
+      },
+    })
+    return result.map((resource) =>
+      Pair.reconstruct(PairId.reconstruct(resource.id), {
+        name: PairName.reconstruct(resource.name),
+        teamId: TeamId.reconstruct(resource.teams[0].id),
+        participantIdList: resource.users.map(({ id }) =>
+          ParticipantId.reconstruct(id)
+        ),
+      })
+    )
   }
   async getPairListInTeam(): Promise<Pair[]> {
     // TODO:
---
+++ src/infra/pair/tests/pair-repository.integration.test.ts
@@ -0,0 +1,100 @@
+import { createContext } from "../../shared/context"
+import { PairRepository } from "../pair-repository"
+import { Pair } from "../../../domain/pair/pair"
+import { PairId } from "../../../domain/pair/pair-id"
+import { PairName } from "../../../domain/pair/pair-name"
+import { TeamId } from "../../../domain/team/team-id"
+import { ParticipantId } from "../../../domain/participant/participant-id"
+
+describe(`PairRepository`, () => {
+  const context = createContext()
+
+  let pairRepository: PairRepository
+  beforeEach(() => {
+    pairRepository = new PairRepository(context)
+  })
+
+  beforeAll(async () => {
+    await context.prisma.pair.create({
+      data: {
+        id: "id-pair-a",
+        name: "a",
+        teams: {
+          create: {
+            id: "id-team-1",
+            name: "1",
+          },
+        },
+        users: {
+          create: [
+            {
+              id: "01",
+              name: "01",
+              email: "01@example.com",
+            },
+            {
+              id: "02",
+              name: "02",
+              email: "02@example.com",
+            },
+          ],
+        },
+      },
+    })
+    await context.prisma.pair.create({
+      data: {
+        id: "id-pair-b",
+        name: "b",
+        teams: {
+          connect: {
+            id: "id-team-1",
+          },
+        },
+        users: {
+          create: [
+            {
+              id: "03",
+              name: "03",
+              email: "03@example.com",
+            },
+            {
+              id: "04",
+              name: "04",
+              email: "04@example.com",
+            },
+          ],
+        },
+      },
+    })
+  })
+
+  afterAll(async () => {
+    await context.prisma.pair.deleteMany()
+    await context.prisma.$disconnect()
+  })
+
+  describe(`getAllPairList()`, () => {
+    it(`ペアのリストを返す`, async () => {
+      const actual = await pairRepository.getAllPairList()
+      const expected = [
+        Pair.reconstruct(PairId.reconstruct("id-pair-a"), {
+          name: PairName.reconstruct("a"),
+          teamId: TeamId.reconstruct("id-team-1"),
+          participantIdList: [
+            ParticipantId.reconstruct("01"),
+            ParticipantId.reconstruct("02"),
+          ],
+        }),
+        Pair.reconstruct(PairId.reconstruct("id-pair-b"), {
+          name: PairName.reconstruct("b"),
+          teamId: TeamId.reconstruct("id-team-1"),
+          participantIdList: [
+            ParticipantId.reconstruct("03"),
+            ParticipantId.reconstruct("04"),
+          ],
+        }),
+      ]
+      expect(actual).toStrictEqual(expected)
+    })
+  })
+})
