0205466fe66207b735019efbb928a48a26c35f1c
sushidesu <hugemerge@gmail.com>
2021-11-10 20:32:17 +0900

    空きのあるペアを取得するdomain serviceを追加

---
+++ src/domain/pair/get-vacant-pair-list.ts
@@ -0,0 +1,14 @@
+import { DomainService } from "../shared/domainService"
+import { Pair } from "./pair"
+import { IPairRepository } from "./interface/pair-repository"
+
+export class GetVacantPairList extends DomainService<"get-vacant-pair-list"> {
+  constructor(private readonly pairRepository: IPairRepository) {
+    super()
+  }
+
+  async do(): Promise<readonly Pair[]> {
+    const allPairs = await this.pairRepository.getAllPairList()
+    return allPairs.filter((pair) => pair.canAcceptParticipant())
+  }
+}
