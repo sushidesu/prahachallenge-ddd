5ae26fd927a2e0cb08da2de891d887fc8d22dd64
sushidesu <hugemerge@gmail.com>
2021-11-15 18:02:31 +0900

    impl get-parent-team

--- src/domain/pair/get-parent-team.ts
+++ src/domain/pair/get-parent-team.ts
@@ -1,14 +1,14 @@
 import { DomainService } from "../shared/domainService"
 import { Pair } from "./pair"
 import { Team } from "../team/team"
+import { ITeamRepository } from "../team/interface/team-repository"
 
 export class GetParentTeam extends DomainService<"get-parent-team"> {
-  constructor() {
+  constructor(private readonly teamRepository: ITeamRepository) {
     super()
   }
 
   async do(pair: Pair): Promise<Team | undefined> {
-    console.log(pair)
-    return undefined
+    return this.teamRepository.getTeamByPair(pair.id)
   }
 }
