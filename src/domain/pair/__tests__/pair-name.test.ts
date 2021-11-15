5cd546277facf5e370f706aefb175bba68f47035
sushidesu <hugemerge@gmail.com>
2021-11-15 22:40:51 +0900

    fix test

--- src/domain/pair/__tests__/pair-name.test.ts
+++ src/domain/pair/__tests__/pair-name.test.ts
@@ -3,23 +3,21 @@ import { PairName } from "../pair-name"
 describe("PairName", () => {
   describe("ペアの名前の長さは1文字", () => {
     it(`名前を "a" にすると正しく作成できる`, () => {
-      const pairName = PairName.createFromFactory("a")
+      const pairName = PairName.create("a")
       expect(pairName).toEqual(expect.any(PairName))
     })
     it("名前を空にするとエラーになる", () => {
-      expect(() => PairName.createFromFactory("")).toThrowError(
+      expect(() => PairName.create("")).toThrowError(
         "pair-name cannot be empty"
       )
     })
     it(`名前が2文字以上の場合エラーになる`, () => {
-      expect(() => PairName.createFromFactory("aa")).toThrowError(
-        "pair-name is too long"
-      )
+      expect(() => PairName.create("aa")).toThrowError("pair-name is too long")
     })
   })
   describe("ペアの名前は英小文字のみ", () => {
     it(`名前に大文字が含まれている場合エラーになる`, () => {
-      expect(() => PairName.createFromFactory("A")).toThrowError(
+      expect(() => PairName.create("A")).toThrowError(
         "pair-name cannot be uppercase"
       )
     })
