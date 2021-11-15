0720fb6dc0f8d8cf9793b4a1fa1fe23fde6a49cd
sushidesu <hugemerge@gmail.com>
2021-11-15 18:39:34 +0900

    fix test

--- src/domain/pair/__tests__/get-parent-team.test.ts
+++ src/domain/pair/__tests__/get-parent-team.test.ts
@@ -1,15 +1,22 @@
+import { mock } from "jest-mock-extended"
+import { GetParentTeam } from "../get-parent-team"
 import { Team } from "../../team/team"
 import { TeamId } from "../../team/team-id"
 import { TeamName } from "../../team/team-name"
-import { GetParentTeam } from "../get-parent-team"
 import { Pair } from "../pair"
 import { PairId } from "../pair-id"
 import { PairName } from "../pair-name"
+import { ITeamRepository } from "../../team/interface/team-repository"
 
 describe(`GetParentTeam`, () => {
+  const teamRepositoryMock = mock<ITeamRepository>()
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
   let getParentTeam: GetParentTeam
   beforeEach(() => {
-    getParentTeam = new GetParentTeam()
+    getParentTeam = new GetParentTeam(teamRepositoryMock)
   })
 
   const pair = Pair.reconstruct(PairId.reconstruct("a"), {
@@ -19,16 +26,29 @@ describe(`GetParentTeam`, () => {
   })
 
   it(`親のチームを取得する`, async () => {
-    const actual = getParentTeam.do(pair)
+    // arrange
+    teamRepositoryMock.getTeamByPair.mockResolvedValue(
+      Team.reconstruct(TeamId.reconstruct("1"), {
+        name: TeamName.reconstruct("1"),
+        participantIdList: [],
+      })
+    )
     const expected = Team.reconstruct(TeamId.reconstruct("1"), {
       name: TeamName.reconstruct("1"),
       participantIdList: [],
     })
+    // act
+    const actual = await getParentTeam.do(pair)
+    // assert
     expect(actual).toEqual(expected)
   })
 
   it(`親のチームがない場合、undefinedを返す`, async () => {
+    // arrange
+    teamRepositoryMock.getTeamByPair.mockResolvedValue(undefined)
+    // act
     const actual = await getParentTeam.do(pair)
+    // assert
     expect(actual).toBe(undefined)
   })
 })
