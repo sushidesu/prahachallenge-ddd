c79658a05ce48f2ec1e0c15eee030d9b64f0d6f0
sushidesu <hugemerge@gmail.com>
2021-11-17 23:12:49 +0900

    idでペアを取得するメソッドをrepositoryに追加

--- src/domain/pair/interface/pair-repository.ts
+++ src/domain/pair/interface/pair-repository.ts
@@ -1,9 +1,11 @@
 import { Pair } from "../pair"
+import { PairId } from "../pair-id"
 import { TeamId } from "../../team/team-id"
 
 export interface IPairRepository {
   insert(pair: Pair): Promise<void>
   update(pair: Pair): Promise<void>
+  getPairById(pairId: PairId): Promise<Pair | undefined>
   getAllPairList(): Promise<Pair[]>
   getPairListInTeam(teamId: TeamId): Promise<Pair[]>
 }
