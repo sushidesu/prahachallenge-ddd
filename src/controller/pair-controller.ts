028824451fb8fe9276cd0df293b0043cd4b6d004
sushidesu <hugemerge@gmail.com>
2021-11-18 18:45:42 +0900

    join-pair-usecaseをpair-controllerに移動

--- src/controller/pair-controller.ts
+++ src/controller/pair-controller.ts
@@ -1,14 +1,27 @@
 import { RequestHandler } from "express"
+import { JoinPairUsecase } from "../usecase/participant/join-pair-usecase/join-pair-usecase"
+import { JoinPairInputData } from "../usecase/participant/join-pair-usecase/join-pair-input-data"
 
 export class PairController {
+  constructor(private joinPairUsecase: JoinPairUsecase) {}
+
   public join: RequestHandler = async (req, res, next) => {
     try {
-      console.log(req.body)
+      const { participantId } = req.body
+      if (typeof participantId !== "string") {
+        throw new Error("participantId is required")
+      }
+      const inputData = new JoinPairInputData({
+        participantId,
+      })
+      await this.joinPairUsecase.exec(inputData)
+      res.json({ message: "success!" })
     } catch (err) {
+      console.error(err)
       if (err instanceof Error) {
         res.status(500).json({ message: err.message })
       } else {
-        res.status(500).json({ message: "unexpected error occurred" })
+        res.status(500).json({ message: "unexpected error occurred." })
       }
     } finally {
       next()
