51ab787f64c746724de87dd86f9c41664cdad1a4
sushidesu <hugemerge@gmail.com>
2021-11-18 19:20:44 +0900

    参加者を新規作成するユースケースを実装

--- src/config/participant-router.ts
+++ src/config/participant-router.ts
@@ -1,13 +1,26 @@
 import { Router } from "express"
 import { ParticipantController } from "../controller/participant-controller"
 import { CreateParticipantUsecase } from "../usecase/participant/create-participant-usecase/create-participant-usecase"
+import { createContext } from "../infra/shared/context"
+import { ParticipantRepository } from "../infra/participant/participant-repository"
+import { CheckEmailAlreadyExists } from "../domain/participant/domain-service/check-email-already-exists"
+import { ParticipantFactory } from "../domain/participant/participant-factory"
 
 // repository
+const context = createContext()
+const participantRepository = new ParticipantRepository(context)
 
 // domain-service
+const checkEmailAlreadyExists = new CheckEmailAlreadyExists(
+  participantRepository
+)
+const participantFactory = new ParticipantFactory(checkEmailAlreadyExists)
 
 // usecase
-const createParticipantUsecase = new CreateParticipantUsecase()
+const createParticipantUsecase = new CreateParticipantUsecase(
+  participantRepository,
+  participantFactory
+)
 
 // controller
 const controller = new ParticipantController(createParticipantUsecase)
--- src/usecase/participant/create-participant-usecase/create-participant-usecase.ts
+++ src/usecase/participant/create-participant-usecase/create-participant-usecase.ts
@@ -1,12 +1,20 @@
 import { CreateParticipantInputData } from "./create-participant-input-data"
+import { ParticipantFactory } from "../../../domain/participant/participant-factory"
+import { IParticipantRepository } from "../../../domain/participant/interface/participant-repository"
 
+/**
+ * 参加者の作成
+ */
 export class CreateParticipantUsecase {
-  /**
-   * 参加者の作成
-   */
-  exec(inputData: CreateParticipantInputData): void {
-    console.log(inputData)
+  constructor(
+    private participantRepository: IParticipantRepository,
+    private participantFactory: ParticipantFactory
+  ) {}
+  async exec(inputData: CreateParticipantInputData): Promise<void> {
+    const { name, email } = inputData.props
     // 参加者エンティティを生成
+    const participant = await this.participantFactory.create({ name, email })
     // 保存
+    await this.participantRepository.insert(participant)
   }
 }
