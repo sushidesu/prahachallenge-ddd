884b2f0261af4f496646bf07f90147b660872913
sushidesu <hugemerge@gmail.com>
2021-11-20 01:41:15 +0900

    ペアと参加者を取得するクエリサービスを定義

---
+++ src/usecase/pair/get-pair-list/interface/pair-with-participant-query-service-interface.ts
@@ -0,0 +1,8 @@
+import { PairWithParticipantQueryCommand } from "./pair-with-participant-query-command"
+import { PairWithParticipantDTO } from "./pair-with-participant-DTO"
+
+export interface IPairWithParticipantQueryService {
+  query(
+    command: PairWithParticipantQueryCommand
+  ): Promise<PairWithParticipantDTO[]>
+}
