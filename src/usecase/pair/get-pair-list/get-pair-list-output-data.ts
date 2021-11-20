f29809f8e21c4a5bb0258000012350cd01a8905b
sushidesu <hugemerge@gmail.com>
2021-11-20 12:27:00 +0900

    ペア一覧を取得するユースケースの返り値を定義

--- src/usecase/pair/get-pair-list/get-pair-list-output-data.ts
+++ src/usecase/pair/get-pair-list/get-pair-list-output-data.ts
@@ -1,1 +1,23 @@
-export class GetPairListOutputData {}
+interface GetPairListOutputDataProps {
+  id: string
+  name: string
+  participants: readonly ParticipantSummary[]
+}
+
+interface ParticipantSummary {
+  id: string
+  name: string
+}
+export class GetPairListOutputData {
+  constructor(private readonly props: GetPairListOutputDataProps) {}
+
+  get id(): string {
+    return this.props.id
+  }
+  get name(): string {
+    return this.props.name
+  }
+  get participants(): readonly ParticipantSummary[] {
+    return this.props.participants
+  }
+}
